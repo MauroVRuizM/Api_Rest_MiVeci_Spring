d7c1274885e999dfff18abaa488f3a276eef7d43
MauroVRuizM <59812652+MauroVRuizM@users.noreply.github.com>
2020-12-29 17:06:44 -0500

    Initial Code

---
+++ src/main/java/com/miVeci/backend/ApiMiVeciApplication.java
@@ -0,0 +1,13 @@
+package com.miVeci.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMiVeciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMiVeciApplication.class, args);
+	}
+
+}
