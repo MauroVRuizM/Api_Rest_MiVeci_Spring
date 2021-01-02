b757782e0f97648e054ae177587ce7aff8b94740
MauroVRuizM <59812652+MauroVRuizM@users.noreply.github.com>
2021-01-02 16:56:48 -0500

    Listining deleted

--- src/main/java/com/miVeci/backend/models/dao/IListing.java
+++
@@ -1,9 +0,0 @@
-package com.miVeci.backend.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.miVeci.backend.models.entities.Listing;
-
-public interface IListing extends CrudRepository<Listing, Long> {
-
-}
