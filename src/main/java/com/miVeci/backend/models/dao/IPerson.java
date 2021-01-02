d73d0377803bb82fd906797b2174c9aa2a04bcff
Chris <paul19992012@gmail.com>
2021-01-02 17:55:47 -0500

    Controllers, Services, Models ( Person, Customer, Order, Payment)

--- src/main/java/com/miVeci/backend/models/dao/IPerson.java
+++
@@ -1,9 +0,0 @@
-package com.miVeci.backend.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.miVeci.backend.models.entities.Person;
-
-public interface IPerson extends CrudRepository<Person, Long> {
-
-}
